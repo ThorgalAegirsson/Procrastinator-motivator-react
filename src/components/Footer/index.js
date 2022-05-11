f5210c6adc198ce07bcc2cec8987e49c999e13a0
Radek Szumski <usa.rs@live.com>
2022-05-11 16:40:26 +0100

    UPDATE footer layout

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,8 +6,10 @@ const Footer = (props) => {
 
     return (
         <footer className={classes.footer}>
-            <p>Data: <a href="https://www.worldbank.org" target="_blank" rel='noreferrer'>World Bank</a></p>
-            <p>Source code: <a href="https://github.com/ThorgalAegirsson/Procrastinator-motivator-react" target="_blank" rel='noreferrer'>Github</a></p>
+            <div className={classes.info}>
+                <p>Data: <a href="https://www.worldbank.org" target="_blank" rel='noreferrer'>World Bank</a></p>
+                <p>Source code: <a href="https://github.com/ThorgalAegirsson/Procrastinator-motivator-react" target="_blank" rel='noreferrer'>Github</a></p>
+            </div>
             <p>&copy; 2022 <a href="https://room33.dev" target='_blank' rel='noreferrer'>Room33</a></p>
         </footer>
     );
